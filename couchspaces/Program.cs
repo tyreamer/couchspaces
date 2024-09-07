3cfb64fbe915bfc9ec4cfca35683b2fffb9a1716
tyreamer <tyreamer@gmail.com>
2024-09-07 13:54:09 -0400

    added mudblazor and cleaned up main page

--- couchspaces/Program.cs
+++ couchspaces/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using couchspaces.Services;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Authorization;
 using couchspaces;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,8 +15,7 @@ builder.Services.AddScoped<TokenValidationService>();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<CouchspacesAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CouchspacesAuthenticationStateProvider>());
-
-// Register authorization services
+builder.Services.AddMudServices();
 builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
